faeb78299d8723d252f53b9208800a09115b26cb
abhay333d <abhaydandge333@gmail.com>
2024-09-01 01:35:10 +0530

    by using the simplex noise created tow noises, noise of origin and noise of target and mixed both on the basis of uProgress. Hence, got the better transition

--- src/shaders/particles/fragment.glsl
+++ src/shaders/particles/fragment.glsl
@@ -1,4 +1,4 @@
-
+varying vec3 vColor;
 
 void main()
 {
@@ -6,7 +6,7 @@ void main()
     float distanceToCenter = length(uv - 0.5);
     float alpha = 0.05 / distanceToCenter - 0.1;
 
-    gl_FragColor = vec4(1.0, 1.0, 1.0, alpha);
+    gl_FragColor = vec4(vColor, alpha);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
--- src/shaders/particles/vertex.glsl
+++ src/shaders/particles/vertex.glsl
@@ -4,13 +4,23 @@ uniform float uProgress;
 
 attribute vec3 aPositionTarget;
 
+varying vec3 vColor;
+
 #include ../includes/simplexNoise3d.glsl
 
 void main()
 {
     //Mixed position
-    float noise = simplexNoise3d(position);
-    float progress = uProgress;
+    float noiseOrigin = simplexNoise3d(position * 0.2);
+    float noiseTagert = simplexNoise3d(aPositionTarget * 0.2);
+    float noise = mix(noiseOrigin, noiseTagert, uProgress);
+    noise = smoothstep(-1.0, 1.0, noise);
+
+    float duration = 0.4;
+    float delay = (1.0 - duration) * noise;
+    float end = delay + duration;
+
+    float progress = smoothstep(delay, end, uProgress);
     vec3 mixedPosition = mix(position, aPositionTarget, progress);
 
 
@@ -23,4 +33,7 @@ void main()
     // Point size
     gl_PointSize = uSize * uResolution.y;
     gl_PointSize *= (1.0 / - viewPosition.z);
+
+    //varyings 
+    vColor = vec3(noise);
 }
